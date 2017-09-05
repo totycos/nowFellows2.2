3415c3173f3f30c92b42f91d9c0fa63e1e5dc0f9
totycos <dv94@hotmail.fr>
2017-09-06 01:53:26 +0200

    probleme loop sign in facebook, a cause de la page sign in defini comme root

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users, :controllers => { :omniauth_callbacks => "user/omniauth_callbacks" }
+
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
